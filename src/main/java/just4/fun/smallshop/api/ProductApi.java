1b30bda5bf4fff2d24adf10959ca3fc120966f57
vitaliy-zinchenko <zinjvi@gmail.com>
2015-01-23 22:43:47 +0200

    changes

--- src/main/java/just4/fun/smallshop/api/ProductApi.java
+++ src/main/java/just4/fun/smallshop/api/ProductApi.java
@@ -1,16 +1,10 @@
 package just4.fun.smallshop.api;
 
-import just4.fun.smallshop.beans.Product;
 import just4.fun.smallshop.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import zinjvi.controller.BaseRestController;
-import zinjvi.service.Service;
-
-import java.util.List;
 
 @Controller
 @RequestMapping(value = "/product")
